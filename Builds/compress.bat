234040da54a6d7d108687a33c82ec58ec1d58c51
MJSD <math_jon207@hotmail.com>
2024-02-25 21:01:32 -0600

    build: included compression script to the build directory
    The compress.bat script is meant to compress the builds prior to release.

---
+++ Builds/compress.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo Compressing 4 files...
+echo Progress: 0/4
+
+7z a -tzip -mx=9 "MineSweeper_Windows_x32.zip" .\MineSweeper_x32.exe && del .\MineSweeper_x32.exe
+echo Progress: 1/4
+
+7z a -tzip -mx=9 "MineSweeper_Windows_x64.zip" .\MineSweeper_x64.exe && del .\MineSweeper_x64.exe
+echo Progress: 2/4
+
+7z a -tzip -mx=9 "MineSweeper_Linux_x32.zip" .\MineSweeper_x32.x86_32 && del .\MineSweeper_x32.x86_32
+echo Progress: 3/4
+
+7z a -tzip -mx=9 "MineSweeper_Linux_x64.zip" .\MineSweeper_x64.x86_64 && del .\MineSweeper_x64.x86_64
+echo Progress: 4/4
+
+echo Finished.
+set /p DUMMY=Hit ENTER to exit...
